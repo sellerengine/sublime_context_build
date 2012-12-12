26b895aa893d5ab1fb4da8f2dbaf90df47fa0757
Walt Woods <walt@sellerengine.com>
2012-12-12 11:39:19 -0800

    Basing python path off of project settings, nonblocking.

--- ContextBuild.py
+++ ContextBuild.py
@@ -1,13 +1,16 @@
+
+from __future__ import print_function
+
 import sublime, sublime_plugin
 
 import os
 import re
 import shlex
 import subprocess
+import threading
+import time
 
 options = sublime.load_settings('ContextBuild.sublime-settings')
-
-pythonPath = os.environ.get(options.get('pythonEnvironmentVariable', ''), '')
 
 class Build(object):
     last = None
@@ -19,48 +22,132 @@ class Build(object):
         if onlyLastFailures:
             with open(".noseids", "r") as f:
                 self.noseFailureData = f.read()
+        self.thread = None
+
+
+    def abort(self):
+        while self.thread:
+            self.shouldStop = True
+            time.sleep(0.1)
 
 
     def run(self):
-        cmd = "nosetests"
+        self.shouldStop = False
+        self.thread = threading.Thread(target = self._realRun)
+        self.thread.daemon = True
+        self.thread.start()
+
+
+    def _realRun(self):
+        oldBuild = getattr(self.window, '_ContextBuildRunning', None)
+        if oldBuild:
+            oldBuild.abort()
+
+        self.window._ContextBuildRunning = self
+        try:
+            self._doBuild()
+        finally:
+            self.window._ContextBuildRunning = None
+            self.thread = None
+
+
+    def _doBuild(self):
+        cmd = "nosetests -v"
+        testDesc = None
         if self.paths:
-            print(self.paths)
+            testDesc = repr(self.paths)
             cmd += " " + " ".join([ p.encode('utf8') for p in self.paths ])
         elif self.noseFailureData is not None:
             with open(".noseids", "w") as f:
                 f.write(self.noseFailureData)
             cmd += " --failed"
+            testDesc = "failed tests"
         elif self.tests:
-            print(self.tests)
+            testDesc = repr(self.tests)
             cmd += " " + " ".join([ p.encode('utf8') for p in self.tests ])
         else:
             print("No tests to run.")
             return
             
         env = os.environ.copy()
-        env['PYTHONPATH'] = pythonPath
+        env['PYTHONPATH'] = self._option('context_build_python_path')
+
+        print("Running tests: " + testDesc)
+
         p = subprocess.Popen(shlex.split(cmd), stdout = subprocess.PIPE, 
                 stderr = subprocess.STDOUT, env = env,
                 universal_newlines = True)
-        stdout, _ = p.communicate()
-        print stdout
-        print("Result: " + str(p.poll()))
+        stdThread = threading.Thread(target = self._dumpStdout, args = (p,))
+        stdThread.start()
+        while p.poll() is None:
+            if self.shouldStop:
+                break
+            time.sleep(0.1)
+        if p.poll() is None:
+            print("Aborting tests...")
+            while p.poll() is None:
+                try:
+                    p.terminate()
+                except OSError, e:
+                    # Died already
+                    pass
+                time.sleep(0.1)
 
 
-class ContextBuildCurrentCommand(sublime_plugin.TextCommand):
-    def run(self, editor):
-        Build.last = Build(paths = [ self.view.file_name() ])
-        Build.last.run()
+    def _dumpStdout(self, p):
+        """Dumps the stdout from subprocess p; called in a new thread."""
+        while p.poll() is None:
+            p.stdout.flush()
+            buffer = []
+            while True:
+                l = p.stdout.read(1)
+                if not l:
+                    break
+                print(l, end = '')
+            time.sleep(0.1)
+        print(p.stdout.read())
+
+
+    def _option(self, name, default = ''):
+        """We want to use the project's overloaded settings if they're 
+        available for things like paths, but default to sane defaults
+        specified in ContextBuild.sublime-settings.
+        """
+        return self.window.active_view().settings().get(name, 
+                options.get(name, default))
+
+
+class ContextBuildPlugin(sublime_plugin.WindowCommand):
+    def hasLastBuild(self):
+        return getattr(self.window, '_ContextBuild', None) is not None
+
+
+    @property
+    def lastBuild(self):
+        return self.window._ContextBuild
+
+
+    @lastBuild.setter
+    def lastBuild(self, value):
+        value.window = self.window
+        self.window._ContextBuild = value
+
+
+class ContextBuildCurrentCommand(ContextBuildPlugin):
+    def run(self):
+        self.lastBuild = Build(paths = 
+                [ self.window.active_view().file_name() ])
+        self.lastBuild.run()
 
 
     def is_enabled(self):
         return True
 
 
-class ContextBuildSelectedCommand(sublime_plugin.WindowCommand):
+class ContextBuildSelectedCommand(ContextBuildPlugin):
     def run(self, paths = []):
-        Build.last = Build(paths)
-        Build.last.run()
+        self.lastBuild = Build(paths)
+        self.lastBuild.run()
 
 
     def is_enabled(self):
@@ -76,9 +163,7 @@ def findTestFromLine(view, testLine, actualStart, testsOut):
         if sel.a > actualStart:
             continue
         text = view.substr(sel)
-        print(repr(text))
         clsIndent = len(re.match("[ \t]*", text).group())
-        print("Comparing " + str(clsIndent))
         if clsIndent < indent:
             # MATCH!
             testsOut.append(view.file_name() + ':' 
@@ -87,7 +172,7 @@ def findTestFromLine(view, testLine, actualStart, testsOut):
             break
 
 
-class ContextBuildSelectionCommand(sublime_plugin.WindowCommand):
+class ContextBuildSelectionCommand(ContextBuildPlugin):
     def run(self):
         view = self.window.active_view()
         reg = view.sel()[0]
@@ -112,26 +197,34 @@ class ContextBuildSelectionCommand(sublime_plugin.WindowCommand):
                     break
 
         if tests:
-            print(tests)
-            Build.last = Build(tests = tests)
-            Build.last.run()
+            self.lastBuild = Build(tests = tests)
+            self.lastBuild.run()
         else:
             print("No tests found")
 
 
-class ContextBuildLastCommand(sublime_plugin.WindowCommand):
+class ContextBuildLastCommand(ContextBuildPlugin):
     def run(self):
-        Build.last.run()
+        self.lastBuild.run()
 
 
     def is_enabled(self):
-        return Build.last is not None
+        return self.hasLastBuild()
 
 
-class ContextBuildFailuresCommand(sublime_plugin.WindowCommand):
+class ContextBuildFailuresCommand(ContextBuildPlugin):
     def run(self):
-        Build.last = Build(onlyLastFailures = True)
-        Build.last.run()
+        self.lastBuild = Build(onlyLastFailures = True)
+        self.lastBuild.run()
 
     def is_enabled(self):
-        return Build.last is not None
+        return self.hasLastBuild()
+
+
+class ContextBuildStopCommand(ContextBuildPlugin):
+    def run(self):
+        self.lastBuild.abort()
+
+
+    def is_enabled(self):
+        return self.hasLastBuild() and self.lastBuild.thread
